85560d234aaad9b835632b6d1f017ebe0e4b3bb8
MulherMarav <101612046+MulherMarav@users.noreply.github.com>
2024-05-13 17:18:27 -0300

    Adição das dependencias

---
+++ src/main/kotlin/com/mulhermarav/pokedexreativo/PokedexReativoApplication.kt
@@ -0,0 +1,11 @@
+package com.mulhermarav.pokedexreativo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PokedexReativoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PokedexReativoApplication>(*args)
+}
